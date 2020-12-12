4f95011d35fe65a70e6c7f0758242c45956e84c3
anand <hello@newnoiseworks.com>
2020-12-11 22:19:48 -0500

    ignores some files for build

--- Utils/GameConfig.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-const real_world_seconds_per_day = 2 * 60
-const nakama_host = "127.0.0.1"
-const nakama_key = "defaultkey"
-const nakama_port = 7350
-const nakama_secure = false
-const version = "0.1.33"
