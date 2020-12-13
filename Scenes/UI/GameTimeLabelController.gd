9196716ea392e47763fde076659049c3d6a22449
anand <hello@newnoiseworks.com>
2020-12-13 18:25:27 -0500

    updates time more often

--- Scenes/UI/GameTimeLabelController.gd
+++ Scenes/UI/GameTimeLabelController.gd
@@ -5,7 +5,7 @@ var in_game_minute: int
 
 
 func _physics_process(_delta: float):
-	if in_game_hour != GameTime.in_game_hour && in_game_minute != GameTime.in_game_minute:
+	if in_game_hour != GameTime.in_game_hour || in_game_minute != GameTime.in_game_minute:
 		in_game_hour = GameTime.in_game_hour
 		in_game_minute = GameTime.in_game_minute
 		text = GameTime.get_rounded_time()
