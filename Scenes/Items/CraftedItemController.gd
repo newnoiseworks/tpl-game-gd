b1f456193bc8f7f92aaa51c8ed2c03c2c6d3dd14
anand <hello@newnoiseworks.com>
2021-01-10 01:46:49 -0500

    patches opn crafted items

--- Scenes/Items/CraftedItemController.gd
+++ Scenes/Items/CraftedItemController.gd
@@ -2,10 +2,8 @@ extends "res://Scenes/Items/ItemController.gd"
 
 
 func primary_action():
-	print("are we placing yet")
 	place_crafted_item_on_farm_grid()
 
 
 func secondary_action():
-	print("it should be happening")
 	remove_crafted_item_on_farm_grid()
