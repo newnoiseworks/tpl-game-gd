7ce9560db01f3e4dea8b6f60b8842972abc147c5
Anand Mallik <anand@newnoiseworks.com>
2021-01-11 01:59:27 -0500

    adds say hi to baph and learn econ from jkjz missions

--- RootScenes/Farm/MissionScenes/introJKJZController.gd
+++ RootScenes/Farm/MissionScenes/introJKJZController.gd
@@ -19,14 +19,16 @@ func _exit_tree():
 
 func intro_jkjz_finish():
 	var pos = exit.position
-	jkjz_character._move_event({"ping": "0", "x": pos.x, "y": pos.y})
-	jkjz_character.highlight(false)
-	jkjz_character.set_collision_layer_bit(1, false)
-
 	teleporter.position -= Vector2(-9999, 0)
 
 	yield(TPLG.ui.mission_list.finish_mission("introJKJZ"), "completed")
 	yield(TPLG.ui.mission_list.start_mission("sayHiToSakana"), "completed")
+	yield(TPLG.ui.mission_list.start_mission("sayHiToBaph"), "completed")
+	yield(TPLG.ui.mission_list.start_mission("learnEconFromJKJZ"), "completed")
 	yield(TPLG.ui.mission_list.start_mission("visitTown"), "completed")
+
+	jkjz_character._move_event({"ping": "0", "x": pos.x, "y": pos.y})
+	jkjz_character.highlight(false)
+	jkjz_character.set_collision_layer_bit(1, false)
 
 	TPLG.ui.mission_list.reload_missions()
