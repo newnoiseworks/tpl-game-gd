7a4d192a8b893000b59827328b11bb2359251259
Anand Mallik <anand@newnoiseworks.com>
2021-03-29 22:59:16 -0400

    initiates farm with tilled soil

--- RootScenes/Tutorials/Farming/PlantingTutorialController.gd
+++ RootScenes/Tutorials/Farming/PlantingTutorialController.gd
@@ -10,4 +10,13 @@ func _ready():
 	if no_children():
 		return
 
+	farm_grid.data = {
+		"forageItems": [],
+		"plants": [],
+		"craftedItems": [],
+		"groundTiles":
+		{"0": {"0": "tilled"}, "1": {"0": "tilled"}, "2": {"0": "tilled"}, "3": {"0": "tilled"}}
+	}
+
+	farm_grid.call_deferred("draw_things_from_data")
 	farm_grid.setup_collider()
