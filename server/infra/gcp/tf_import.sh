4302516af4c4f0f53907801a27706dbf126fa357
Anand Mallik <ccanandmallik@gmail.com>
2022-10-12 01:38:11 -0400

    adds ip address to import file

--- server/infra/gcp/tf_import.sh
+++ server/infra/gcp/tf_import.sh
@@ -3,10 +3,12 @@
 terraform state rm google_compute_network.vpc_network
 terraform state rm google_compute_firewall.default
 terraform state rm google_compute_instance.nakama_instance
+terraform state rm google_compute_address.nakama_ip_address
 
 terraform import google_compute_network.vpc_network nakama-instance-network
 terraform import google_compute_firewall.default nakama-instance-firewall
 terraform import google_compute_instance.nakama_instance nakama-instance
+terraform import google_compute_address.nakama_ip_address nakama-static-ip-address
 
 terraform refresh
 
