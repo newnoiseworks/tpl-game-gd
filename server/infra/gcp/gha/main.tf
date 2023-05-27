5d15b6514380b1493ac804491ab8d848fd78eb17
New Noise Anand <amallik@newnoiseworks.com>
2023-05-27 13:38:08 -0400

    updates tf resources to add extra roles to service account, adjusts github action workflow accordingly

--- server/infra/gcp/gha/main.tf
+++ server/infra/gcp/gha/main.tf
@@ -1,6 +1,12 @@
-# NOTE: this may not be needed, as this is for a top level one time project
+# name of project on GCP
 variable "gcp_project" {
   type = string
+}
+
+# salt for updating below resources in real time as GCP caches many of them, particularly the WIPP
+variable "gcp_resource_version" {
+  type = number
+	default = 4
 }
 
 # output WI info as needed for GH stuff
@@ -9,14 +15,14 @@ variable "gcp_project" {
 # WI pool
 resource "google_iam_workload_identity_pool" "omgd_wi_pool" {
   project                   = var.gcp_project
-  workload_identity_pool_id = "${var.gcp_project}-ghac-wip"
+  workload_identity_pool_id = "${var.gcp_project}-ghac-wip-${var.gcp_resource_version}"
 }
 
 # WI provider
 resource "google_iam_workload_identity_pool_provider" "omgd_wip_provider" {
   project                            = var.gcp_project
   workload_identity_pool_id          = google_iam_workload_identity_pool.omgd_wi_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = "${var.gcp_project}-ghac-wipp"
+  workload_identity_pool_provider_id = "${var.gcp_project}-ghac-wipp-${var.gcp_resource_version}"
 
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
@@ -32,7 +38,7 @@ resource "google_iam_workload_identity_pool_provider" "omgd_wip_provider" {
 # GCP service account
 resource "google_service_account" "sa" {
   project      = var.gcp_project
-  account_id   = "omgd-${var.gcp_project}-sa"
+  account_id   = "omgd-${var.gcp_project}-sa-${var.gcp_resource_version}"
 }
 
 # IAM perms for service account; at least project creation & service enabling
@@ -41,7 +47,15 @@ data "google_iam_policy" "omgd_project" {
     role = "roles/iam.workloadIdentityUser"
 
     members = [
-      "serviceAccount:omgd-${var.gcp_project}-sa@${var.gcp_project}.iam.gserviceaccount.com",
+      "serviceAccount:omgd-${var.gcp_project}-sa-${var.gcp_resource_version}@${var.gcp_project}.iam.gserviceaccount.com",
+      "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.omgd_wi_pool.name}/attribute.repository/newnoiseworks/tpl-game-gd"
+    ]
+  }
+  binding {
+    role = "roles/iam.serviceAccountUser"
+
+    members = [
+      "serviceAccount:omgd-${var.gcp_project}-sa-${var.gcp_resource_version}@${var.gcp_project}.iam.gserviceaccount.com",
       "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.omgd_wi_pool.name}/attribute.repository/newnoiseworks/tpl-game-gd"
     ]
   }
--- server/infra/gcp/gha/tf_import.sh
+++ server/infra/gcp/gha/tf_import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-terraform import google_iam_workload_identity_pool.omgd_wi_pool projects/tpl-redux-staging/locations/global/workloadIdentityPools/tpl-redux-staging-ghac-wip
-terraform import google_iam_workload_identity_pool_provider.omgd_wip_provider projects/tpl-redux-staging/locations/global/workloadIdentityPools/tpl-redux-staging-ghac-wip/providers/tpl-redux-staging-ghac-wipp
-terraform import google_service_account.sa projects/tpl-redux-staging/serviceAccounts/omgd-tpl-redux-staging-sa@tpl-redux-staging.iam.gserviceaccount.com
+terraform import google_iam_workload_identity_pool.omgd_wi_pool projects/tpl-redux-staging/locations/global/workloadIdentityPools/tpl-redux-staging-ghac-wip-4
+terraform import google_iam_workload_identity_pool_provider.omgd_wip_provider projects/tpl-redux-staging/locations/global/workloadIdentityPools/tpl-redux-staging-ghac-wip-4/providers/tpl-redux-staging-ghac-wipp-4
+terraform import google_service_account.sa projects/tpl-redux-staging/serviceAccounts/omgd-tpl-redux-staging-sa-4@tpl-redux-staging.iam.gserviceaccount.com
 
 terraform refresh
 
