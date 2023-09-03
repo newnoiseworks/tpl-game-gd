b9cd2f71b28cf275520396d889eb0d1e6aeaee4e
New Noise Anand <amallik@newnoiseworks.com>
2023-09-02 23:52:04 -0400

    im guessing this is / was important

--- server/infra/gcp/tf_import.sh
+++ server/infra/gcp/tf_import.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+terraform import google_project.nakama_gcp_project tpl-redux-staging
 terraform import google_compute_network.vpc_network nakama-instance-network
 terraform import google_compute_firewall.default nakama-instance-firewall
 terraform import google_compute_instance.nakama_instance nakama-instance
